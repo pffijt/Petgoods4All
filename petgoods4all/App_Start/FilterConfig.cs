f036dfcb630dd18b881949bb02de260e671442ad
pffijt <33753557+pffijt@users.noreply.github.com>
2018-09-21 21:41:18 +0200

    Product information
    ...

---
+++ petgoods4all/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace petgoods4all
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ petgoods4all/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="petgoods4all.MvcApplication" Language="C#" %>
