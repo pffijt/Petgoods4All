d42e2718dd2d82f6dab9e71afd41cc82f39dccf8
Jordi Suijkerbuijk <j.suijkerbuijk@hotmail.com>
2018-09-25 12:45:53 +0200

    Orderhistory page made
    Made controller for order and account,
    Order history page made,

---
+++ petgoods4all/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace petgoods4all.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Aanmelden()
+        {
+            ViewBag.Message = "Your SignUp page.";
+
+            return View();
+        }
+    }
+}
