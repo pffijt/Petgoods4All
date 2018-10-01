f2375cc73a11db450a8d3bfb43f8f654de2f614e
pffijt <33753557+pffijt@users.noreply.github.com>
2018-10-01 12:21:24 +0200

    Andere Framework

--- petgoods4all/Controllers/AccountController.cs
+++ petgoods4all/Controllers/AccountController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using petgoods4all.Models;
 
 namespace petgoods4all.Controllers
