c864ea7dbb7464ebbfd52a11f37ed4bcd2b0bdfa
Jordi Suijkerbuijk <j.suijkerbuijk@hotmail.com>
2018-10-03 18:00:42 +0200

    Seach func 1.0
    Search sends searched string to controller

---
+++ petgoods4all/Controllers/VoorraadController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using petgoods4all.Models;
+
+namespace petgoods4all.Controllers
+{
+    public class VoorraadController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductBrowsen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProductSearch(string productSearch)
+        {
+            //Db context
+            var db = new ModelContext();
+
+            var result = from voorraad in db.Voorraad select voorraad;
+
+            return View("~/Views/Voorraad/productBrowsen.cshtml");
+        }
+    }
+}
