8846238f15b2b6c25f68fbb7282abdfab26de32d
yungkit9 <yungkithui@gmail.com>
2018-10-28 14:08:50 +0100

    email verificatie done

--- petgoods4all/Controllers/AccountController.cs
+++ petgoods4all/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net.Mail;
 using System.Text;
 using petgoods4all.Models;
+using System.Net;
 
 namespace petgoods4all.Controllers
 {
@@ -65,20 +66,40 @@ namespace petgoods4all.Controllers
 
             db.Account.Add(a);
             db.SaveChanges();
-            SmtpClient client = new SmtpClient();
-            client.Port = 587;
-            client.Host = "smtp.gmail.com";
-            client.EnableSsl = true;
-            client.Timeout = 10000;
-            client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.UseDefaultCredentials = false;
-            client.Credentials = new System.Net.NetworkCredential("petgoods4all@gmail.com", "adminpetgoods4all");
 
-            MailMessage mm = new MailMessage("petgoods4all@gmail.com", email, "test", "test");
-            mm.BodyEncoding = UTF8Encoding.UTF8;
-            mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+            MailMessage message = new System.Net.Mail.MailMessage();
+            string fromEmail = "petgoods4all@gmail.com";
+            string emailPassword = "adminpetgoods4all";
+            message.From = new MailAddress(fromEmail);
+            message.To.Add(email);
+            message.Subject = "Activatie mail";
+            message.Body = @"Klik op de link om uw account te activeren http://localhost:56002/Account/Inloggen";
+            message.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
 
-            client.Send(mm);
+            using (SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587))
+            {
+                smtpClient.EnableSsl = true;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(fromEmail, emailPassword);
+
+                smtpClient.Send(message.From.ToString(), message.To.ToString(), message.Subject, message.Body);
+            }
+
+            //SmtpClient client = new SmtpClient();
+            //client.Port = 587;
+            //client.Host = "smtp.gmail.com";
+            //client.EnableSsl = true;
+            //client.Timeout = 10000;
+            //client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //client.UseDefaultCredentials = false;
+            //client.Credentials = new System.Net.NetworkCredential("petgoods4all@gmail.com", "adminpetgoods4all");
+
+            //MailMessage mm = new MailMessage("petgoods4all@gmail.com", email, "Verificatie email", "test");
+            //mm.BodyEncoding = UTF8Encoding.UTF8;
+            //mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+            //client.Send(mm);
 
             return View("~/Views/Account/Inloggen.cshtml");
         }
