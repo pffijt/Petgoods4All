3e6cb9cccd499c3ae28b220fd4dc7a7fb5c94c4b
Jordi Suijkerbuijk <j.suijkerbuijk@hotmail.com>
2018-10-31 22:27:54 +0100

    ShoppingCart price
    Shopping Cart price werkend

--- petgoods4all/Controllers/VoorraadController.cs
+++ petgoods4all/Controllers/VoorraadController.cs
@@ -263,8 +263,8 @@ namespace petgoods4all.Controllers
                 double a = Convert.ToDouble(item.Prijs);
                 double b = a * item.Kwantiteit;
                 c = c + b;
-                decimal d = Convert.ToDecimal(c / 1);
-                string prijs = d.ToString("0.00");
+                decimal d = Convert.ToDecimal(c / 100);
+                string prijs = d.ToString("00.00");
 
                 ViewBag.Prijs = prijs;
             }
