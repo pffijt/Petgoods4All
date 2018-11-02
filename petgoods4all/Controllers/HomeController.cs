7d659dd16a6618a6794e348c1d3197bb3110c645
yungkit9 <yungkithui@gmail.com>
2018-11-02 18:37:22 +0100

    contactform klaar

--- petgoods4all/Controllers/HomeController.cs
+++ petgoods4all/Controllers/HomeController.cs
@@ -5,7 +5,8 @@ using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using petgoods4all.Models;
-
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace petgoods4all.Controllers
 {
@@ -22,13 +23,36 @@ namespace petgoods4all.Controllers
 
             return View();
         }
-
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contact(string contactEmail, string Onderwerp, string Bericht)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(contactEmail));
+            message.To.Add(new MailboxAddress("petgoods4all@gmail.com"));
+            message.Subject = Onderwerp;
+            message.Body = new TextPart("html")
+            {
+                Text = "From: " + contactEmail + "<br>" +
+                "Onderwerp: " + Onderwerp + "<br>" +
+                "Bericht: " + Bericht
+            };
+
+            using(var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587);
+                client.Authenticate("petgoods4all@gmail.com", "adminpetgoods4all");
+                client.Send(message);
+                client.Disconnect(false);
+            }
+
+            return View("Contact");
+        }
+
 
         public ActionResult Productpage(int identication = 1)
         {
