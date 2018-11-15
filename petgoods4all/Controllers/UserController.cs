1c6ba71e61a31212a0a1e308b8136786e11f4327
elseSL <els_lok@hotmail.com>
2018-11-15 15:01:52 +0100

    klantpage

--- petgoods4all/Controllers/UserController.cs
+++ petgoods4all/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNetCore.Mvc;
@@ -9,10 +10,11 @@ namespace petgoods4all.Controllers
 {
     public class UserController : Controller
     {
-
+        /////////inlog page////////
         // GET: User
-        [HttpGet]
+        ModelContext db = new ModelContext();
 
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
@@ -23,38 +25,94 @@ namespace petgoods4all.Controllers
             return View();
         }
 
+        public ActionResult updateGegevens()
+        {
+            return View();
+        }
+
         public ActionResult UserBeheer()
         {
+             // var UserId = HttpContext.Session.GetInt32("UID");
+            var userId = HttpContext.Session.GetInt32("UID");
+            ViewBag.userId = userId;
+
+            using (db)
+            {
+
+                var model = from acc in db.Account select acc;
+                // var model = from acc in db.Account
+                //             select new Account
+                //     {
+                //         voornaam = acc.voornaam,
+                //         achternaam = acc.achternaam,
+                //         telefoonnummer = acc.telefoonnummer,
+                //         straatnaam = acc.straatnaam,
+                //         password = acc.password,
+                //         email = acc.email
+                //     };
+                ViewBag.AccountData = model.ToList();
+
+            }
             return View();
         }
         
         //
         public ActionResult ReadAccount(string inputEmailUser)
         {
-            var db = new ModelContext();
             Account account = db.Account.Find(inputEmailUser);
 
             return View(account);
         }
 
-        public ActionResult UpdateAccount(string inputEmailUser, string newEmail, string newinputPasswordUser)
-        {
-            var db = new ModelContext();
-            Account account = db.Account.Find(inputEmailUser);
-            account.email = newEmail;
-            account.password = newinputPasswordUser;
-            db.Account.Update(account);
+        public ActionResult UpdateAccount(string inputEmailUser, string originalEmailUser, string inputPasswordUser, string inputVoornaamUser, string inputAchternaamUser, string inputTelefoonnummerUser, string inputStraatnaamUser)
+        {            
+            using (db)
+            {
+                var userId = HttpContext.Session.GetInt32("UID");
+                
+                //var accountUpdate = db.Account.Find(inputEmailUser);// input from inlog, comparing db email.
+                var accountUpdate = db.Account.Where(account => account.id == userId).FirstOrDefault();
+                accountUpdate.voornaam = inputVoornaamUser;
+                accountUpdate.achternaam = inputAchternaamUser;
+                accountUpdate.telefoonnummer = inputTelefoonnummerUser;
+                accountUpdate.straatnaam = inputStraatnaamUser;
+                accountUpdate.password = inputPasswordUser;
+                accountUpdate.email = inputEmailUser;
 
-            return View(account);
+                db.Account.Update(accountUpdate);
+                db.SaveChanges();
+            }
+            return RedirectToAction("UserHome");
+        }
+
+        public ActionResult readAccount()
+        {
+            using (db)
+            {
+
+                var model = from acc in db.Account select acc;
+                // var model = from acc in db.Account
+                //             select new Account
+                //     {
+                //         voornaam = acc.voornaam,
+                //         achternaam = acc.achternaam,
+                //         telefoonnummer = acc.telefoonnummer,
+                //         straatnaam = acc.straatnaam,
+                //         password = acc.password,
+                //         email = acc.email
+                //     };
+                ViewBag.AccountData = model.ToList();
+
+                return View();
+            }
         }
 
         public ActionResult DeleteAccount(string inputEmailUser)
         {
-            var db = new ModelContext();
             Account account = db.Account.Find(inputEmailUser);
             db.Account.Remove(account);
 
             return View();
-        }
+        }        
     }
 }
